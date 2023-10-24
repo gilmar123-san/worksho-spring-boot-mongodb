0d3a36808c34d1b03bb340af63b8219a4eb8150c
gilmar123-san <69655786+gilmar123-san@users.noreply.github.com>
2023-10-23 22:13:50 -0300

    Obtendo post por id

---
+++ src/main/java/com/gilmarsantana/workshopmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.gilmarsantana.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gilmarsantana.workshopmongodb.domain.Post;
+import com.gilmarsantana.workshopmongodb.repository.PostRepository;
+import com.gilmarsantana.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;	
+
+	public Post findById(String id){		
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
